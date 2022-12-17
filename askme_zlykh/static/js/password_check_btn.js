364ba48fa575ef6246414191482de005d6369797
alan zlykh <cigikpugik@gmail.com>
2022-12-17 09:33:22 +0300

    Refactor all pages to look more beautiful.

--- askme_zlykh/static/js/password_check_btn.js
+++
@@ -1,9 +0,0 @@
-$('body').on('click', '.password-checkbox', function(){
-	if ($(this).is(':checked')){
-		$('#password-input-repeat').attr('type', 'text');
-		$('#password-input').attr('type', 'text');
-	} else {
-		$('#password-input-repeat').attr('type', 'password');
-		$('#password-input').attr('type', 'password');
-	}
-});
